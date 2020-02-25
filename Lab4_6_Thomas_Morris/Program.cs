59194fef61dad0a13aef53d719dd87d689fd3172
=tmorrisiii86 <=tmorrisiii86@gmail.com>
2020-02-25 11:46:02 -0600

    Add project files.

---
+++ Lab4_6_Thomas_Morris/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab4_6_Thomas_Morris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Days of the Week Array
+            string[] days =
+            {
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+
+            //Tasks Array
+            string[] tasks = new string[7];
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine("Type in a new task for " + days[i]);
+                tasks[i] = days[i] + " : " + Console.ReadLine();
+            }
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(tasks[i]);
+            }
+        }
+    }
+}
